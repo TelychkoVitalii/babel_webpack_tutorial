205e51c36cca8659e886c555acf9ffc120944064
Vitalii Telychko <vtel@ciklum.com>
2019-03-16 13:32:36 +0200

    second

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+const path = require('path');
+
 module.exports = {
-  entry: ''
-}
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
