2c1c0e02d2d2718d9cb88a1d7425dd3e510f0d07
Vitalii Telychko <vtel@ciklum.com>
2019-03-16 14:24:17 +0200

    forth

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,19 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  // mode: 'development',
+
+  // With 1 entry point
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    // path: path.resolve(__dirname, 'dist')
+  },
   plugins: [
     new HtmlWebpackPlugin({
       filename: './index.html'
     })
   ]
-
-  // With 1 entry point
-  // entry: './src/index.js',
-  // output: {
-  //   filename: 'main.js',
-  //   path: path.resolve(__dirname, 'dist')
-  // }
   // entry: {
   //   pageOne: './src/pageOne/index.js',
   //   pageTwo: './src/pageTwo/index.js',
